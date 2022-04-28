eb65d7aa86963616171b5f3f46718fc983ef48e4
Abdiqani Afrah <abdi@Abdiqanis-MacBook-Air.local>
2022-04-28 11:42:04 +0100

    multiply question completed

---
+++ src/main/scala/Multiply.scala
@@ -0,0 +1,9 @@
+object Multiply extends App {
+
+  // This code does not execute properly. Try to figure out why.
+  //   def multiply(a: Int, b: Int) = a * b
+
+  def multiply(a: Int, b: Int) = a * b
+  println(multiply(5, 5))
+  // needs printLn additionally a * b need values/figures
+}
