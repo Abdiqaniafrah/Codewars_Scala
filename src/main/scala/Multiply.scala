188c619b95461c1fa87c551f24e4c9a711b163cc
Abdiqani Afrah <abdi@Abdiqanis-MacBook-Air.local>
2022-04-28 11:47:00 +0100

    second solution

--- src/main/scala/Multiply.scala
+++ src/main/scala/Multiply.scala
@@ -6,4 +6,9 @@ object Multiply extends App {
   def multiply(a: Int, b: Int) = a * b
   println(multiply(5, 5))
   // needs printLn additionally a * b need values/figures
+
+  // second solution
+  val a = 7
+  val b = 7
+  println(multiply(a, b))
 }
