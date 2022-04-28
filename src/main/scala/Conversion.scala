b784913f5442f8ddd9496a6156d4fd19cedce8e3
Abdiqani Afrah <abdi@Abdiqanis-MacBook-Air.local>
2022-04-28 16:42:42 +0100

    numberToString complete

---
+++ src/main/scala/Conversion.scala
@@ -0,0 +1,13 @@
+object Conversion extends App {
+
+
+  // We need a function that can transform a number into a string.
+  // Example
+  // 123 --> "123"
+  // 999 --> "999"
+  //   def numberToString(n: Int): String = ???
+
+  def numberToString(n: Int): String = "67"
+
+
+}
